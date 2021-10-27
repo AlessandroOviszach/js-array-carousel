f8a137e9c0e3eb8c5f7394f72432607747c9e99a
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-10-27 19:42:51 +0200

    add content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,84 @@
-console.log('JS OK')
+/**
+ * JS CAROUSEL
+ *
+ * Dati tre array contenenti
+ * una lista ordinata di 5 immagini e
+ * una lista ordinata dei relativi 5 luoghi,
+ * e una lista di 5 testi.
+ * creare un carosello come nella foto alegata
+ *
+ * Al click dell'utente sulle frecce l'immagine attiva diventa
+ * visibile in formato grande a sinistra e
+ * nel suo angolo in basso a destra dovranno essere aggiunti i relativi titolo e il testo.
+ * Aggiorniamo anche la thumbnail attiva.
+ */
+
+// Immagini
+const imageCollection = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+];
+
+// Titoli
+const titleCollection = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise',
+];
+
+// Testi
+const textCollection = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+const ref_img = document.querySelector('.images');
+const ref_thumbs = document.querySelector('.thumbs');
+let active_img = 1;
+const prev = document.querySelector('prev');
+const next = document.querySelector('next');
+
+
+for (let i = 0; i < imageCollection.length; i++) {
+    // immagini grandi
+    ref_img.innerHTML +=
+    `<div class="image-container">
+        <img src="${imageCollection[i]}" alt="${titleCollection[i]}">
+        <div class="text">
+            <h3>${titleCollection[i]}</h3>
+            <p>${textCollection[i]}</p>
+        </div>
+    </div>`
+
+    // immagini piccole
+    ref_thumbs.innerHTML +=
+    `<div class="thumb">
+        <img src="${imageCollection[i]}" alt="${titleCollection[i]}">
+    </div>`
+}
+
+// attivazione visualizzazione dell'immagine
+document.getElementsByClassName('image-container')[active_img].classList.add('active');
+
+document.getElementsByClassName('thumb')[active_img].classList.add('active');
+
+prev.addEventListener('click', function(){
+    
+})
+
+
+
+
+
+
+
+
+
